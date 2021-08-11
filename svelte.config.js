dd6a3a069f94eda46263ff17148585a16308e595
philrwebb <prwebb@optusnet.com.au>
2021-08-11 17:34:20 +0800

    preprocess

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,7 @@ import sveltePreprocess from 'svelte-preprocess';
 import adapt from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: sveltePreprocess(),
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		// target: '#svelte',
