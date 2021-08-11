cd0089dc72e9408f180d9c366621889709f7357e
philrwebb <prwebb@optusnet.com.au>
2021-08-11 12:29:20 +0800

    adapter added

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,10 @@
-import node from '@sveltejs/adapter-node';
+import adapt from '@sveltejs/adapter-node';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-		adapter: Node(),
+		// target: '#svelte',
+		adapter: adapt(),
 	}
 };
 
